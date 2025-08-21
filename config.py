60ea44ba40dbd16cbb19b30df34093e7f6733307
shreenavya-mr <shreenavyamr@gmail.com>
2025-08-21 17:39:18 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Sample sensitive information (DO NOT use in real apps!)
+SECRET_USERNAME = "admin"
+SECRET_PASSWORD = "admin123"
+
+API_KEY = "sk_test_51J1234abcdXYZ"
+DB_PASSWORD = "P@ssw0rd123"
+SERVER_IP = "192.168.1.100"
